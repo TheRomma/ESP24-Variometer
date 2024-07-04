fdf99a2c456745446dbb4216a80c25c042b79826
Ville Hokkinen <ville.hokkinen@gmail.com>
2024-07-04 22:48:44 +0300

    Some untested bluetooth stuff
    Needed fixes in the NUS library aswell, maybe try to find something better for writing NMEA protocol.

--- esp24/esp24.ino
+++ esp24/esp24.ino
@@ -5,6 +5,8 @@
 #include "buzzer.hpp"
 #include "display.hpp"
 #include "imu.hpp"
+#include "NuPacket.hpp"
+#include "NimBLEDevice.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define SLEEP_INTERVAL 5 //Sleep interval for updates in ms. Larger intervals should count multiples of SLEEP_INTERVAL.
@@ -86,26 +88,39 @@ void setup() {
 
   val = -199;
   idler = 0;
+  
+  NimBLEDevice::init("SENDING DATA IN NMEA PROTOCOL");
+  NuPacket.start();
 }
 
 void loop() {
-  
-  if (! bmp.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
+  if (NuPacket.connect()) 
+  {
+    NuPacket.send("Data from sensors in NMEA format");
   }
+  else
+  {
+    Serial.println("--Waiting for connection--");
+    while (!NuPacket.connect())
+        delay(500);
+    Serial.println("--Connected--");
+  }
+  // if (! bmp.performReading()) {
+  //   Serial.println("Failed to perform reading :(");
+  //   return;
+  // }
 
-  // printBaroData(&bmp);
-  sBmx160SensorData_t Omagn, Ogyro, Oaccel;
+  // // printBaroData(&bmp);
+  // sBmx160SensorData_t Omagn, Ogyro, Oaccel;
   
-  imu.getAllData(&Omagn, &Ogyro, &Oaccel);
-  // imu.printAllData(&Omagn, &Ogyro, &Oaccel);
+  // imu.getAllData(&Omagn, &Ogyro, &Oaccel);
+  // // imu.printAllData(&Omagn, &Ogyro, &Oaccel);
 
-  //Updates every SLEEP_INTERVAL.
-  idler = (idler+1)%10;
-  if(idler == 0)val++;
-  display.write(val);
-  buzzer.play(val);
+  // //Updates every SLEEP_INTERVAL.
+  // idler = (idler+1)%10;
+  // if(idler == 0)val++;
+  // display.write(val);
+  // buzzer.play(val);
 
-  delay(SLEEP_INTERVAL);
+  // delay(SLEEP_INTERVAL);
 }
