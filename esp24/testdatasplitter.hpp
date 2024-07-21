1e373922c69665b84e101307c30080ff5bbcde55
aseppa20 <aseppa20@student.oulu.fi>
2024-07-21 18:42:43 +0300

    Refactoring quite a bit

--- esp24/testdatasplitter.hpp
+++ esp24/testdatasplitter.hpp
@@ -8,18 +8,9 @@ struct testDataSplitter
 	testDataSplitter();
 	~testDataSplitter();
 
-	float coord_x_acc;
-	float coord_y_acc;
-	float coord_z_acc;
-	float coord_x_gyro;
-	float coord_y_gyro;
-	float coord_z_gyro;
-	float vel_d;
-	float baro;
-
+	uint8_t small_counter;
 	uint16_t counter;
-	uint16_t timestamp;
 
-	void get_test_data(*float x_acc, *float y_acc, *float z_cc, *float x_gyro, *float y_gyro, *float z_gyro, *float barometer, *float up_acc, *uint16_t timestamp);
+	void get_test_data(float *x_acc, float *y_acc, float *z_cc, float *x_gyro, float *y_gyro, float *z_gyro, float *barometer, float *up_acc, uint16_t *timestamp);
 	
 };
