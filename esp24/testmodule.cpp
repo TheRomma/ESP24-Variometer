d523632860c4443359eab81547fb2193efd6eb2d
aseppa20 <aseppa20@student.oulu.fi>
2024-07-22 17:35:00 +0300

    Vector implementation with struct return

---
+++ esp24/testmodule.cpp
@@ -0,0 +1,40 @@
+#include "testmodule.hpp"
+#include "fstream"
+#include "buzzer.hpp"
+#include "display.hpp"
+
+/*
+Selftest module for the project by Aleksanteri Seppä
+*/
+
+Buzzer buzzer;
+Display display;
+
+TestModule::TestModule(Buzzer b, Display d) {
+	buzzer = b;
+	display = d;
+}
+
+TestModule::~TestModule() {
+
+}
+
+void TestModule::startTests(bool bluetooth, bool serial) {
+
+}
+
+void TestModule::BuzzerDisplayTest() {
+
+}
+
+void TestModule::IMUTest() {
+
+}
+
+void TestModule::barometerTest() {
+
+}
+
+void TestModule::testFlight() {
+
+}
---
+++ esp24/testmodule.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cinttypes>
+#include <string>
+
+struct TestModule {
+	TestModule(Buzzer b, Display d);
+	~TestModule();
+
+	void startTests(bool bluetooth, bool serial);
+	void BuzzerDisplayTest();
+	void IMUTest();
+	void barometerTest();
+	void testFlight();
+};
